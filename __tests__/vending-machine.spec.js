fd34c288c012d2a844f0c78fd47fbae7a0990cd0
bsoetarto7 <bsoetarto7@gmail.com>
2017-11-18 20:43:49 -0800

    Added second test when item does not exist

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,14 +1,20 @@
 const vendingMachine = require('../src/vending-machine');
 
 describe('Vending Machine', () => {
-  beforeEach(()=>{
-    test.subject = new vendingMachine();
-  })
+  
   describe('Querying for inventory', () => {
+    beforeEach(()=>{
+      test.subject = new vendingMachine();
+    })
     describe('When inventory exists', () => {
       it('Should tell me the inventory data', () => {
         const result = test.subject.queryInventory('pepsi');
         expect(result.item).toBe('pepsi')
+      })
+    })
+    describe('When inventory does not exist', () => {
+      it('Should throw and error', () => {
+        expect(() => test.subject.queryInventory('orange juice')).toThrow('No such item exist')
       })
     })
   })
